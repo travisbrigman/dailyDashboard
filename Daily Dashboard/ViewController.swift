75d09bc04c17f6bd93badeec074a8620cfbfb828
Jonathan Wiley <wiley@lunarlincoln.com>
2018-12-13 20:59:25 -0600

    Pass location for weather into Weather helper class instead of creating a separate ViewController instance in the Weather helper class.

--- Daily Dashboard/ViewController.swift
+++ Daily Dashboard/ViewController.swift
@@ -55,7 +55,7 @@ class ViewController: UIViewController {
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
         
-        theWeather.getWeatherData { (currentWeather) in
+        theWeather.getWeatherData(for: self.appleTVlocation()) { (currentWeather) in
             print(currentWeather)
         }
     }
